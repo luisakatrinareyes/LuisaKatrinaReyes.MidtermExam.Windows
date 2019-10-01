eb25898fbab16a9557efac32b6fa6dca62f4cbd8
Luisa Katrina Reyes <luisakatrinareyes@gmail.com>
2019-10-01 18:45:14 +0800

    Completing the Code

--- LuisaKatrinaReyes.MidtermExam/LuisaKatrinaReyes.MidtermExam.Windows/Form2.xaml.cs
+++ LuisaKatrinaReyes.MidtermExam/LuisaKatrinaReyes.MidtermExam.Windows/Form2.xaml.cs
@@ -11,17 +11,78 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace LuisaKatrinaReyes.MidtermExam.Windows
 {
-    /// <summary>
-    /// Interaction logic for Form2.xaml
-    /// </summary>
-    public partial class Form2 : Window
+        
+        public partial class Form2 : Window
     {
-        public Form2()
+        string item;
+        public Form2(string whatimlookingfor)
         {
             InitializeComponent();
+            this.item = whatimlookingfor;
+        }
+        public class Menu
+        {
+            public title[] categorys { get; set; }
+        }
+
+        public class title
+        {
+            public string name { get; set; }
+            [JsonProperty("menu-items")]
+            public mItems[] mItems { get; set; }
+        }
+
+        public class mItems
+        {
+            public string name { get; set; }
+            [JsonProperty("Sub-items")]
+            public sItems[] sItems { get; set; }
+            public string description { get; set; }
+        }
+
+        public class sItems
+        {
+            public string name { get; set; }
+            public string price { get; set; }
+            public dis discount { get; set; }
+        }
+
+        public class dis
+        {
+            public string amount { get; set; }
+        }
+
+        private void Windows_Loaded(object sender, RoutedEventArgs e)
+        {
+            var theitem = item;
+            var testing = JsonConvert.DeserializeObject<Menu>(File.ReadAllText(System.AppDomain.CurrentDomain.BaseDirectory + "restomenu.json"));
+            string find = null;
+            int length = testing.categorys.GetLength(0);
+            for (int x = 0; x < length; ++x)
+            {
+                int length2 = testing.categorys[x].mItems.GetLength(0);
+                for (int y = 0; y < length2; y++)
+                {
+                    int length3 = testing.categorys[x].mItems[y].sItems.GetLength(0);
+                    for (int z = 0; z < length3; z++)
+                    {
+                        find = testing.categorys[x].name + " - " + testing.categorys[x].mItems[y].name + " - " + testing.categorys[x].mItems[y].sItems[z].name;
+                        if (find == item)
+                        {
+                            lblName.Content = "Product: " + testing.categorys[x].mItems[y].name;
+                            lblPrice.Content = "Price: " + testing.categorys[x].mItems[y].sItems[z].price;
+                            lblDesc.Content = "Desc: " + testing.categorys[x].mItems[y].description;
+                            lblDiscount.Content = "Discount: " + testing.categorys[x].mItems[y].sItems[z].discount.amount;
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- LuisaKatrinaReyes.MidtermExam/LuisaKatrinaReyes.MidtermExam.Windows/MainWindow.xaml.cs
+++ LuisaKatrinaReyes.MidtermExam/LuisaKatrinaReyes.MidtermExam.Windows/MainWindow.xaml.cs
@@ -20,25 +20,26 @@ namespace LuisaKatrinaReyes.MidtermExam.Windows
 
     public class Menu
     {
-        public title categorys { get; set; }
+        public title[] categorys { get; set; }
     }
 
     public class title
     {
         public string name { get; set; }
         [JsonProperty("menu-items")]
-        public string mItems { get; set; }
+        public mItems[] mItems { get; set; }
     }
 
     public class mItems
     {
         public string name { get; set; }
         [JsonProperty("Sub-items")]
-        public string sItems { get; set; }
+        public sItems[] sItems { get; set; }
     }
 
     public class sItems
     {
+        public string name { get; set; }
         public string price { get; set; }
     }
     public partial class MainWindow : Window
@@ -50,7 +51,63 @@ namespace LuisaKatrinaReyes.MidtermExam.Windows
 
         private void BtnButton_Click(object sender, RoutedEventArgs e)
         {
+            var area = JsonConvert.DeserializeObject<Menu>(File.ReadAllText(System.AppDomain.CurrentDomain.BaseDirectory + "restomenu.json" ));
+            int length = area.categorys.GetLength(0);
 
+            for (int x = 0; x < length; x++)
+            {
+                TreeViewItem ParentItem = new TreeViewItem();
+                ParentItem.Header = area.categorys[x].name;
+                TreeView1.Items.Add(ParentItem);
+
+                int length2 = area.categorys[x].mItems.GetLength(0);
+
+                for (int y = 0; y < length2; y++)
+                {
+                    TreeViewItem Child1Item = new TreeViewItem();
+                    Child1Item.Header = area.categorys[x].name + " - " + area.categorys[x].mItems[y].name;
+                    ParentItem.Items.Add(Child1Item);
+
+                    int length3 = area.categorys[x].mItems[y].sItems.GetLength(0);
+
+                    for (int z = 0; z < length3; z++)
+                    {
+                        TreeViewItem SubItem = new TreeViewItem();
+                        SubItem.Header = area.categorys[x].name + " - " + area.categorys[x].mItems[y].name + " - " + area.categorys[x].mItems[y].sItems[z].name;
+                        Child1Item.Items.Add(SubItem);
+                    }
+                }
+            }
+        }
+
+        private void TreeView1_MouseDoubleClick (object sender, MouseButtonEventArgs e)
+        {
+            var testing = JsonConvert.DeserializeObject<Menu>(File.ReadAllText(System.AppDomain.CurrentDomain.BaseDirectory + "restomenu.json"));
+            var selected = TreeView1.SelectedItem as TreeViewItem;
+            var whatimlookingfor = selected.Header.ToString();
+
+
+            string find = null;
+            int length = testing.categorys.GetLength(0);
+            for (int x = 0; x < length; ++x)
+            {
+                int length2 = testing.categorys[x].mItems.GetLength(0);
+                for (int y = 0; y < length2; y++)
+                {
+                    int length3 = testing.categorys[x].mItems[y].sItems.GetLength(0);
+                    for (int z = 0; z < length3; z++)
+                    {
+                        find = testing.categorys[x].name + " - " + testing.categorys[x].mItems[y].name + " - " + testing.categorys[x].mItems[y].sItems[z].name;
+                        if (find == whatimlookingfor)
+                        {
+
+                            Form2 newWindow = new Form2(whatimlookingfor);
+                            newWindow.Show();
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
